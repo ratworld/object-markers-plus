26f8b89a1c23b629811cd3c908b7ef3bb5103e26
jay <jayvoigt0@gmail.com>
2023-08-24 02:45:26 -0400

    reorganize project and change package info

--- src/test/java/net/runelite/client/plugins/objectindicators/ExamplePluginTest.java
+++ src/test/java/net/runelite/client/plugins/objectindicators/ExamplePluginTest.java
@@ -1,4 +1,4 @@
-package com.example;
+package net.runelite.client.plugins.objectindicators;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
